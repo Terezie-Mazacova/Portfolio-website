e987e6a6cdf52679ac88f00e239cadc084ed3fca
Terezie Mazáčová <terezkamazacova@post.cz>
2024-11-15 23:59:16 +0100

    hotová celá sekce Faq

---
+++ script.js
@@ -0,0 +1,9 @@
+// == SECTION FAQ ==
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle ('active')
+    })
+})
+
