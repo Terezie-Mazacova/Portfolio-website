ed5297c863b5b993597eb72ff111344874e21853
Terezie Mazáčová <terezkamazacova@post.cz>
2024-11-16 12:42:57 +0100

    staženy ikony, html sekce Testimonials - použití slideru ze splide.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// == SLIDER ==
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide',{
+        perPage: 3,
+        gap: '20px',
+        arrows: true,
+        type: 'loop',
+        //height: '400px',
+        breakpoints: {
+            1200: {
+                perPage: 3,
+                gap: '15px',
+                //height: '550px'
+            },
+            992: {
+                perPage: 2,
+                gap: '10px',
+                //height: '450px'
+                
+            },
+            768: {
+                perPage: 1,
+                gap: '5px',
+                //height: '450px'
+            }
+        }
+    } );
+    splide.mount();
+});
+
 // == SECTION FAQ ==
 const toggles = document.querySelectorAll('.faq-toggle')
 
