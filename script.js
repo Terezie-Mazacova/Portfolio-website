b9bd9234bb67a1842602626382789fd54596ed57
Terezie Mazáčová <terezkamazacova@post.cz>
2024-12-25 13:46:41 +0100

    Menší upravy v header.scss

--- script.js
+++ script.js
@@ -10,35 +10,9 @@ hamburger.addEventListener('click', function() {
 
 
 // == SLIDER ==
-/*
-document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.splide',{
-        perPage: 3,
-        gap: '30px',
-        pagination: false,
-        arrows: true,
-        type: 'loop',
-        breakpoints: {
-            1280: {
-                perPage: 2,
-                gap: '20px',
-
-                
-            },
-            1023: {
-                perPage: 1,
-            },
-        }
-    } );
-    splide.mount();
-});
-*/
-
 document.addEventListener('DOMContentLoaded', function () {
-    // Vyhledání všech elementů s třídou .splide
     const splides = document.querySelectorAll('.splide');
 
-    // Pro každý nalezený element vytvořte novou instanci Splide
     splides.forEach(function (splideElement) {
         new Splide(splideElement, {
         perPage: 3,
@@ -69,4 +43,3 @@ toggles.forEach(toggle => {
         toggle.parentNode.classList.toggle ('active')
     })
 })
-
